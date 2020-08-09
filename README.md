d2cdf6246dff61eb5b02a7eaa4bd381e0473bf5b
Christian M <christian.mtr@gmail.com>
2020-08-09 13:29:51 -0500

    email to login

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is a Django project template with all necessary stuff to run a functional p
 
 - Organization model with some basic attributes.
 - User model (from Abstract model) with foreign key to Organization model.
+- User's email to login (`username` field is copied from email).
 
 ## Usage
 
@@ -16,19 +17,32 @@ You must have installed Django on your system to use `django-admin` command.
 django-admin startproject --template https://github.com/christianmtr/my_django_template/archive/master.zip new_django_project .
 ```
 
-2. Change the database backend in `settings.py` at line 72 and it parameters.
+2. Create and active an virtual environment:
+```
+python -m venv venv
+source venv/bin/activate
+```
+> Only works with Python 3.X, use `python3` if is necessary.
 
-3. Create migration files:
+3. Change the database backend in `settings.py` at line 72 and it parameters.
+
+4. Install dependencies from `requirements.txt` by running:
+```
+pip install -r requirements.txt 
+```
+> Install database driver if its necessary.
+ 
+5. Create migration files:
 ```
 python manage.py makemigrations
 ```
 
-4. Apply migrations:
+6. Apply migrations:
 ```
 python manage.py migrate
 ```
-5. Run project:
 
+7. Run project:
 ```
 python manage.py runserver
 ```
--- core/models.py
+++ core/models.py
@@ -1,5 +1,36 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        email = self.normalize_email(email)
+        user = self.model(email=email.lower(), username=email.lower(), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_staff', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class Organization(models.Model):
@@ -14,6 +45,12 @@ class Organization(models.Model):
 
 class User(AbstractUser):
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.get_full_name()
