8500eaf74d82990c93689bc9259a491cd05fc45f
Christian M <christian.mtr@gmail.com>
2020-08-09 20:01:49 -0500

    fix shared apps and custom token serializer

--- project_name/settings.py
+++ project_name/settings.py
@@ -35,6 +35,11 @@ SHARED_APPS = [
     'core',
 
     'django.contrib.contenttypes',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
 ]
 
 TENANT_APPS = [
